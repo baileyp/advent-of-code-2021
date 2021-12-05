20ab286d54f21a036134f146e0773ed7381e0959
Peter Bailey <b33tle@gmail.com>
2021-12-05 12:21:57 -0400

    Refactor of Day 3, Part 1 to better accommodate Part 2

--- src/solution/day03.js
+++ src/solution/day03.js
@@ -1,5 +1,31 @@
 const { range } = require("../lib/functions");
 
+/**
+ * Given a list of binary numbers as strings, determine the "polarity" of each bit position.
+ *
+ * Polarity is determined "counting cards" style, where a negative value indicates that 0 was the most common
+ * bit, a positive value indicates that 1 was the most common bit, and 0 indicates they were equally common.
+ *
+ * @param {string[]}  binaryList  An array of binary numbers as strings
+ * @returns {number[]}            An array of polarities where the first bit is in the nth position
+ */
+function binaryListToPolarityArray(binaryList) {
+  const numbers = binaryList.map(bin => parseInt(bin, 2));
+
+  const bitWidth = binaryList[0].length;
+  const counts = new Array(bitWidth).fill(0);
+
+  numbers.forEach(entry => {
+    for (let width of range(0, bitWidth - 1)) {
+      const mask = 2 ** width;
+      const bitIsSet = (entry & mask) === mask;
+      counts[width] += bitIsSet ? 1 : -1;
+    }
+  });
+
+  return counts.reverse();
+}
+
 module.exports = {
   /**
    * O(n * w) time and O(w) space where n is the quantity of numbers in the report and w is the width of the
@@ -9,25 +35,18 @@ module.exports = {
    * @returns {number}
    */
   part1: function(input) {
-    const diagnosticReport = input.split("\n")
-      .map(bin => parseInt(bin, 2));
+    const diagnosticReport = input.split("\n");
+    const counts = binaryListToPolarityArray(diagnosticReport);
 
-    const bitWidth = input.split("\n").pop().length;
-    const epsilonMask = 2 ** bitWidth - 1;
-    const counts = new Array(bitWidth).fill(0);
-
-    diagnosticReport.forEach(entry => {
-      for (let width of range(0, bitWidth - 1)) {
-        const mask = 2 ** width;
-        const bitIsSet = (entry & mask) === mask;
-        counts[width] += bitIsSet ? 1 : -1;
-      }
-    });
-
-    const gammaRateBinary = counts.reverse().map(polarity => polarity > 0 ? '1' : '0').join('');
-    const gammaRate = parseInt(gammaRateBinary, 2)
-    const epsilonRate = gammaRate ^ epsilonMask;
+    const gammaRateBinary = counts.map(polarity => polarity > 0 ? '1' : '0').join('');
+    const epsilonRateBinary = counts.map(polarity => polarity < 0 ? '1' : '0').join('');
+    const gammaRate = parseInt(gammaRateBinary, 2);
+    const epsilonRate = parseInt(epsilonRateBinary, 2);
 
     return gammaRate * epsilonRate;
   },
+
+  part2: function(input) {
+    const diagnosticReport = input.split("\n");
+  }
 };
